1df43fef129b921540015cf8f5df4addaf6bd83e
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-05-03 19:52:47 +0900

    sample_ex1-1：参照渡し

---
+++ sample_ex1-1/sample_ex1-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void ref( int );
+void print( int );
+
+int main(){
+    int n = 5;
+    print( n );
+    // QÆ“n‚µ
+    ref( n );
+    print( n );
+    return 0;
+}
+
+void ref( int n ){
+    n = 1;
+}
+
+void print( int n ){
+    cout << "n=" << n << endl;
+}
