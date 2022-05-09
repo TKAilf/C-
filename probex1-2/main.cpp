b26925683f0cad5072f3a2200ee4d9947ab12d50
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-05-09 19:45:05 +0900

    probex1-1 probex1-2 probex1-3

---
+++ probex1-2/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+void changeNumber( int& );
+ 
+int main(){
+    int n = 5;
+    cout << "変更前 : " << n << endl;
+    changeNumber( n );
+    cout << "変更後 : " << n << endl;
+ 
+}
+ 
+void changeNumber( int& n ){
+    n *= 2;    //  数値
+}
+
+
+// 変数渡しだと下記になる
+// #include <iostream>
+ 
+// using namespace std;
+ 
+// void changeNumber(int*);
+ 
+// int main(){
+//     int n = 5;
+//     cout << "変更前 : " << n << endl;
+//     changeNumber(&n);
+//     cout << "変更後 : " << n << endl;
+ 
+// }
+ 
+// void changeNumber(int* n){
+//     *n *= 2;    //  数値
+// }
